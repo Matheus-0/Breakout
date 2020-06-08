ea3b1f1e3f9439aa22320ea9e0e7aaf4db961359
Matheus Alves <scriptermatt@gmail.com>
2020-06-08 18:25:16 -0300

    Create constants.lua

---
+++ src/constants.lua
@@ -0,0 +1,10 @@
+-- Size of our actual window
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+-- Size we're trying to emulate with push
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+-- Paddle movement speed
+PADDLE_SPEED = 200
